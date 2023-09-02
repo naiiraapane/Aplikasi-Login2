541226ac403ba1275f3852a772419a01a1fc68ee
naiiraapane <143243132+naiiraapane@users.noreply.github.com>
2023-09-02 20:26:05 +0700

    Update login-script.js
    update JSDoc

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -1,14 +1,10 @@
-/*
- * Catatan:
- * Gunakan fungsi goToHome() untuk menampilkan halaman home
- * Gunakan fungsi goToLogin() untuk menampilkan halaman login
- * Gunakan fungsi showPopUp() untuk menampilkan pop up error
- */
-
+/** @module Login-Script */
+/** 
 * Membuat variabel loginFormElement untuk tampilan form.
 * @constant {HTMLElement}
 */
 const loginFormElement = document.querySelector('#loginForm');
+
 
 /** 
 * Membuat variabel inputEmailElement untuk tampilan input email.
@@ -16,11 +12,13 @@ const loginFormElement = document.querySelector('#loginForm');
 */
 const inputEmailElement = document.querySelector('#inputEmail');
 
+
 /** 
 * Membuat variabel inputPasswordElement untuk tampilan input password.
 * @constant {HTMLElement}
 */
 const inputPasswordElement = document.querySelector('#inputPassword');
+
 
 /** 
  * Membuat variabel expectedEmail untuk menyimpan informasi email sementara.
@@ -28,11 +26,14 @@ const inputPasswordElement = document.querySelector('#inputPassword');
  */
 const expectedEmail = 'admin@dicoding.com';
 
+
 /** 
  * Membuat variabel expectedPassword untuk menyimpan informasi password sementara.
  * @constant {string}
  */
 const expectedPassword = 'superpassword';
+
+
 
 /* Comment :  Menambahkan aksi klik pada button. */
 loginFormElement.addEventListener('submit', function(event) {
@@ -49,13 +50,17 @@ loginFormElement.addEventListener('submit', function(event) {
    * @constant {string}
    */
   const password = inputPasswordElement.value;
+  
 
   /* Comment : Memastikan bahwa nilai email dan password sesuai dengan nilai yang tersimpan. */
   if (email == expectedEmail && password == expectedPassword) {
     /* Comment : Jika sesuai maka program akan berpindah ke halaman home. */
     goToHome();
+    
   } else {
+    
     /* Comment : Namun jika tidak sesuai maka akan menampilkan informasi bahwa input salah. */
     showPopUp();
+    
   }
 });
